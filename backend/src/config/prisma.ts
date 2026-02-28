7341550a81e24e6572c75cfcc784ea70048ebf6a
fatihahmetcan23 <207318222+fatihahmetcan23@users.noreply.github.com>
2026-02-28 02:39:35 +0100

    feat(db): implement prisma singletone for database connection

---
+++ backend/src/config/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+// Prevent multiple instances in development mode (hot reload)
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = 
+    globalForPrisma.prisma || 
+    new PrismaClient ({
+        log: ['query', 'error', 'warn'], // displays all SQL commands in the terminal
+    });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
