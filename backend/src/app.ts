90ee24f40d513770b9a1cb232e7f78f893a8b87a
fatihahmetcan23 <207318222+fatihahmetcan23@users.noreply.github.com>
2026-03-06 02:49:55 +0100

    feat(chore): mount customer routes and finalize app configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,20 +2,29 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { globalErrorHandler } from './middleware/errorMiddleware';
 import { AppError } from './utils/AppError';
+import customerRouter from './routes/customerRoutes';
 
 const app: Application = express();
 
+// Global MIddleware
 app.use(cors());
 app.use(express.json({ limit: '10kb'}));
 
+// ROUTES
+app.use('/customers', customerRouter)
+
+// Health check remains for monitoring
 app.get('/health-check', ( req: Request, res: Response ) => {
     res.status(200).json({ status: 'success', message: 'API is running'})
 });
 
+
+//UNHANDLE ROUTES
 app.all('/{*path}', ( req: Request, res: Response, next ) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+// GLOBAL ERROR HANDLING MIDDLEWARE
 app.use(globalErrorHandler);
 
 export default app;
